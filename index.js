f5a896b9401d0f88339e73bc2b25e6929d70794f
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-20 19:41:19 -0700

    commit tree 2f189feabd6baa37166ef43b116c13a4c7680192

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = name =>
+  cats.push(name);
+
+const destructivelyPrependCat = name =>
+cats.unshift(name);
+
+const destructivelyRemoveLastCat = name =>
+cats.pop(name);
+
+const destructivelyRemoveFirstCat = name =>
+cats.shift(name);
+
+const appendCat = name => {
+  return [...cats, name ]}
+
+const prependCat = name => {
+  return [name, ...cats]}
+
+
+  const removeFirstCat = () => {
+    return cats.slice(1)}
+  
+  const removeLastCat = () => {
+    return cats.slice(0, cats.length - 1)
+  }
+//   const removeLastCat = () => {
+//   const copyOfCats = cats.slice(1);
+//   return copyOfCats;
+// }
+
+// // const removeFirstCat = name => {
+// //   return cats.slice(0, cats.length - 1)}
+
+
